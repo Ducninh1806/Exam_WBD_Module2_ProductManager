e651e58d2846fffdf2ece04819cb252377501d7f
ducninh1806 <ducninh1806@gmail.com>
2019-10-14 15:22:17 +0700

    cau hinh bai thi Product

---
+++ src/main/java/com/codegym/model/Product.java
@@ -0,0 +1,23 @@
+package com.codegym.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String name;
+    private int amount;
+
+
+
+
+
+
+}
